0d1e1fc8390198557a3c94276d42860e9334b0ae
sametcl2 <sametsahinn01@gmail.com>
2024-08-14 12:59:38 +0300

    removed suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import { Suspense } from 'react'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from '@rneui/themed'
 import { navigationRef } from 'routes/RootNavigation'
 import { store } from 'store/store'
-import { Loading } from 'components/elements/Loading/Loading'
 import { theme } from 'theme'
 import { initI18N } from 'lang/i18n'
 import { AppInitializer } from 'components/AppInitiliazer'
@@ -16,16 +14,14 @@ initI18N()
 export default function App() {
   return (
     <SafeAreaProvider>
-      <Suspense fallback={<Loading />}>
-        <ThemeProvider theme={theme}>
-          <Provider store={store}>
-            <NavigationContainer ref={navigationRef}>
-              <AppInitializer />
-              <Toast />
-            </NavigationContainer>
-          </Provider>
-        </ThemeProvider>
-      </Suspense>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <NavigationContainer ref={navigationRef}>
+            <AppInitializer />
+            <Toast />
+          </NavigationContainer>
+        </Provider>
+      </ThemeProvider>
     </SafeAreaProvider>
   )
 }
