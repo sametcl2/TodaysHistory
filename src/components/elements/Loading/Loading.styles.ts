bff8271d317732f72a8126a71d775b4dd22538ff
Utku Turkoglu <turkoglu.utku@gmail.com>
2024-09-09 00:10:53 +0300

    add new loading

--- src/components/elements/Loading/Loading.styles.ts
+++ src/components/elements/Loading/Loading.styles.ts
@@ -1,10 +1,24 @@
 import { createStyles } from 'theme'
 
-export const useLoadingStyles = createStyles(({ gaps }) => ({
+export const useLoadingStyles = createStyles(({ gaps, colors }) => ({
   loadingContainer: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
     marginBottom: gaps.xxlg
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
+  },
+  ring: {
+    position: 'absolute',
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    borderColor: colors.teal,
+    borderWidth: 10
   }
 }))
--- src/components/elements/Loading/Loading.tsx
+++ src/components/elements/Loading/Loading.tsx
@@ -1,15 +1,56 @@
-import { memo } from 'react'
-import { ActivityIndicator, View } from 'react-native'
-
+import React, { useEffect } from 'react'
+import { View } from 'react-native'
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withRepeat,
+  withTiming
+} from 'react-native-reanimated'
 import { useLoadingStyles } from './Loading.styles'
 
-export const LoadingComponent: React.FC = () => {
+type RingProps = {
+  delay: number
+}
+
+const Ring: React.FC<RingProps> = ({ delay }) => {
   const styles = useLoadingStyles()
+
+  const ring = useSharedValue(0)
+
+  const ringStyle = useAnimatedStyle(() => ({
+    opacity: 0.8 - ring.value,
+    transform: [
+      {
+        scale: interpolate(ring.value, [0, 1], [0, 4])
+      }
+    ]
+  }))
+  useEffect(() => {
+    ring.value = withDelay(
+      delay,
+      withRepeat(
+        withTiming(1, {
+          duration: 4000
+        }),
+        -1,
+        false
+      )
+    )
+  }, [])
+  return <Animated.View style={[styles.ring, ringStyle]} />
+}
+
+export const Loading = () => {
+  const styles = useLoadingStyles()
+
   return (
-    <View style={styles.loadingContainer}>
-      <ActivityIndicator />
+    <View style={styles.container}>
+      <Ring delay={0} />
+      <Ring delay={1000} />
+      <Ring delay={2000} />
+      <Ring delay={3000} />
     </View>
   )
 }
-
-export const Loading = memo(LoadingComponent)
