4cbb10676017cdaa3bb768784971bac2b6dcb9ca
Utku Turkoglu <turkoglu.utku@gmail.com>
2024-09-22 14:14:37 +0300

    fix lint

--- src/components/elements/Loading/Loading.tsx
+++ src/components/elements/Loading/Loading.tsx
@@ -38,6 +38,7 @@ const Ring: React.FC<RingProps> = ({ delay }) => {
         false
       )
     )
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   return <Animated.View style={[styles.ring, ringStyle]} />
 }
